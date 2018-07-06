451096d3fff99ebb0f8c228d3871992219d9225e
Yoshiki Honda <y-honda@ist.osaka-u.ac.jp>
2018-07-06 16:04:09 +0900

    ひな形生成

---
+++ main.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+int add(int a, int b);
+int minus(int a, int b);
+int multi(int a, int b);
+int divide(int a, int b);
+
+int main(){
+  printf("hello world\n");
+  printf("10 + 2 = %d\n",add(10, 2));
+  printf("10 - 2 = %d\n",minus(10, 2));
+  printf("10 * 2 = %d\n",multi(10, 2));
+  printf("10 / 2 = %d\n",divide(10, 2));
+}
+
+// 足し算
+int add(int a, int b){
+  // TODO:
+  return 0;
+}
+
+// 引き算
+int minus(int a, int b){
+  // TODO:
+  return 0;
+}
+
+// 掛け算
+int multi(int a, int b){
+  // TODO:
+  return 0;
+}
+
+// 割り算
+int divide(int a, int b){
+  // TODO:
+  return 0;
+}
